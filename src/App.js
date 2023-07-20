018105b692058bdf5a4b7d7d461d36cf3c18d212
Sirpip91 <allenbradley91@hotmail.com>
2023-07-20 16:40:37 -0500

    testing props

--- src/App.js
+++ src/App.js
@@ -1,11 +1,25 @@
 import './App.css';
 
 function App() {
+  const name = <h1>Bradley</h1>;
+  const age = <h2>22</h2>
+  const email = <h2>hello@gmail.com</h2>
   return (
     <div className="App">
-      Hello world
+    <User name="Bradley" age={23} color="Blue"/>
+     
     </div>
   );
 }
 
+const User = (props) => {
+  return (
+    <div>
+      <h1>{props.name}</h1>
+      <h1>{props.age}</h1>
+      <h1>{props.color}</h1>
+    </div>
+  );
+}
+ 
 export default App;
